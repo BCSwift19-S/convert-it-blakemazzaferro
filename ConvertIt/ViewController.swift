728c4501ebef2b8714c7459aa15f66137f306943
Blake Mazzaferro <blake.mazzaferro@gmail.com>
2019-02-24 16:52:52 -0500

    6.3 Complete

--- ConvertIt/ViewController.swift
+++ ConvertIt/ViewController.swift
@@ -18,6 +18,9 @@ class ViewController: UIViewController {
     
     var formulaArray = ["miles to kilometers", "kilometers to miles", "feet to meters", "yards to meters", "meters to feet", "meters to yards"]
 
+    var fromUnits = ""
+    var toUnits = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         formulaPicker.delegate = self
@@ -43,7 +46,11 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource{
         return formulaArray[row]
     }
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        fromUnitsLabel.text = formulaArray[row]
+        let unitsArray = formulaArray[row].components(separatedBy: " to ")
+        fromUnits = unitsArray[0]
+        toUnits = unitsArray[1]
+        fromUnitsLabel.text = fromUnits
+        resultsLabel.text = toUnits
     }
     
 }
